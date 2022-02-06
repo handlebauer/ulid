682d951399bcea15ca51fabea6c6cf2a48c794d5
Donald Geddes <hi@donaldgeddes.ca>
2022-02-06 14:54:53 -0800

    Fix incorrect return scope

--- src/get-prng.js
+++ src/get-prng.js
@@ -3,9 +3,11 @@ import crypto from 'crypto'
 export const getPRNG = () => {
   if (typeof window !== 'undefined') {
     const crypto = window.crypto || window.msCrypto
-    const buffer = new Uint8Array(1)
-    crypto.getRandomValues(buffer)
-    return () => buffer[0] / 0xff
+    return () => {
+      const buffer = new Uint8Array(1)
+      crypto.getRandomValues(buffer)
+      return buffer[0] / 255
+    }
   }
   if (typeof crypto !== 'undefined') {
     return () => crypto.randomBytes(1).readUInt8() / 0xff
