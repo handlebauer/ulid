39e35f0bef5abf7f4fbef54d27ff5b63f3fb1b4f
Donald Geddes <hi@donaldgeddes.ca>
2022-02-06 14:36:35 -0800

    Reorder PRNG checks to catch browser scenario first

--- src/get-prng.js
+++ src/get-prng.js
@@ -1,14 +1,14 @@
 import crypto from 'crypto'
 
 export const getPRNG = () => {
-  if (typeof crypto !== 'undefined') {
-    return () => crypto.randomBytes(1).readUInt8() / 0xff
-  }
   if (typeof window !== 'undefined') {
     const crypto = window.crypto || window.msCrypto
     const buffer = new Uint8Array(1)
     crypto.getRandomValues(buffer)
     return () => buffer[0] / 0xff
+  }
+  if (typeof crypto !== 'undefined') {
+    return () => crypto.randomBytes(1).readUInt8() / 0xff
   }
   throw new Error('Failed to find a reliable PRNG')
 }
